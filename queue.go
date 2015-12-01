3fa3ec39eb5ccd214c1ad20ba3194afd202360ee
QuantumNightmare <quantumnightmare@hotmail.com>
2015-12-01 15:26:41 +1300

    Kludge - plain queue name

--- queue.go
+++ queue.go
@@ -18,7 +18,7 @@ const (
 	connectionQueueUnackedTemplate   = "rmq::connection::{connection}::queue::[{queue}]::unacked"   // List of deliveries consumers of {connection} are currently consuming
 
 	queuesKey             = "rmq::queues"                     // Set of all open queues
-	queueReadyTemplate    = "rmq::queue::[{queue}]::ready"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
+	queueReadyTemplate    = "{queue}"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
 	queueRejectedTemplate = "rmq::queue::[{queue}]::rejected" // List of rejected deliveries from that {queue}
 
 	phConnection = "{connection}" // connection name
